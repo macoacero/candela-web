7b68030a6c894ef10ad1ae1cf86196e54c58e4e0
Maco Acero <macoacero@gmail.com>
2024-07-30 11:21:31 -0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
         EMAIL_USER: process.env.NEXT_PUBLIC_EMAIL_USER,
         EMAIL_PASS: process.env.NEXT_PUBLIC_EMAIL_PASS,
       },
+      images: {
+        unoptimized: true,
+      },
       output: 'export',
 };
 export default nextConfig;
